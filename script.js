51402db3bdbb3fb60740fe692d95066c25059f79
lcoelho142 <lcoelho142@gmail.com>
2025-09-24 21:09:01 -0400

    Organized Layout & Buttons

---
+++ script.js
@@ -0,0 +1,10 @@
+const shopBtn = document.getElementById('shop-btn');
+const fitBtn = document.getElementById('fit-btn');
+
+shopBtn.addEventListener('click', function() { 
+    alert('loading...');
+});
+
+fitBtn.addEventListener('click', function () {
+    alert('loading...');
+});
